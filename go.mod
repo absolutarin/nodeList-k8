561942cfc07c45d0a56963c271d2a48887c114d9
Arindam Chanda <achanda@vlocity.com>
2020-02-07 22:52:42 +0530

    first

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/absolutarin/nodeList-k8
+
+go 1.12
+
+require (
+	github.com/imdario/mergo v0.3.8 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	k8s.io/api v0.17.2 // indirect
+	k8s.io/apimachinery v0.17.2
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20200124190032-861946025e34 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	nodes, err := listnodes()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(nodes)
+}
+
+func listnodes() ([]string, error) {
+	var nodes []string
+	kubeconfig := flag.String("kubeconfig", os.Getenv("HOME")+"/.kube/config", "path to the kubeconfig file to use")
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nodes, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nodes, err
+	}
+	nodelist, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
+	if err != nil {
+		return nodes, err
+	}
+	for _, n := range nodelist.Items {
+		nodes = append(nodes, n.GetName())
+	}
+	return nodes, nil
+}
